55a2696ffd6ad43d1e76584514ddc8cc1daef7af
NeverGround <167948341+NeverGround@users.noreply.github.com>
2024-05-28 22:25:42 +0900

    Delete day11 directory

--- day11/destrictiromg/array.js
+++
@@ -1,8 +0,0 @@
-// [destructuring]:분해 > array.js
-
-const days = ["월", "화", "수", "목", "금", "토", "일"];
-// const [monday, ...rest] = days;
-
-// const [a, b, c, ...rest] = days; // rest = 목,금,토,일
-
-//const [a, b, c, rest] = days; // rest = 목
--- day11/module/kitchen.js
+++
@@ -1,5 +0,0 @@
-import { makeBread } from "./bread.js";
-import { makeCoffee } from "./coffee.js";
-
-console.log(makeCoffee("아라비카"));
-console.log(makeBread("글루텐푸리"));
