5d074686f8a1116a88e6b001c7b51def94f12e63
NeverGround <167948341+NeverGround@users.noreply.github.com>
2024-06-04 12:54:04 +0900

    Add files via upload

---
+++ 홍석환Javascript/1/dagym.js
@@ -0,0 +1,131 @@
+export function dagym() {
+  return [
+    {
+      code: 0,
+      result: {
+        homeEvent: [
+          {
+            imageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/M_6월배너_02.png",
+            webImageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/pc_6월배너_03.png",
+            isExternalUrl: false,
+            redirectUrl: "https://bit.ly/3uWvSh8",
+          },
+          {
+            imageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/M_%EB%8B%A4%EC%A7%90_%EB%B8%94%EB%A1%9C%EA%B7%B8%EB%A6%AC%EB%B7%B0_%EC%9D%B4%EB%B2%A4%ED%8A%B8_01.png",
+            webImageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/pc_%EB%8B%A4%EC%A7%90_%EB%B8%94%EB%A1%9C%EA%B7%B8%EB%A6%AC%EB%B7%B0_%EC%9D%B4%EB%B2%A4%ED%8A%B8_01.png",
+            isExternalUrl: false,
+            redirectUrl: "https://bit.ly/3xTdqaS",
+          },
+          {
+            imageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/M_메인배너_07.png",
+            webImageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/pc_메인배너_07.png",
+            isExternalUrl: false,
+            redirectUrl: "https://bit.ly/49Y2abH",
+          },
+          {
+            imageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/2023_re-registraion_ver2_mobile.png",
+            webImageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/pc_메인배너_03.png",
+            isExternalUrl: false,
+            redirectUrl: "https://bit.ly/41K1jb3",
+          },
+          {
+            imageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/M_메인배너_2405.png",
+            webImageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/pc_메인배너_2405.png",
+            isExternalUrl: false,
+            redirectUrl: "https://www.da-gym.co.kr/friend",
+          },
+          {
+            imageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/2024_consult_mobile.png",
+            webImageUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/events/2024_consult_pc.png",
+            isExternalUrl: false,
+            redirectUrl: "https://bit.ly/3V2Ma2x",
+          },
+        ],
+        gyms: [
+          {
+            id: "62ecab03f9efad001d90bc08",
+            gymName: "부평 3세트짐",
+            listImgUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/1826c7c068238e39cb/4K8gHar7QpSvsgqithgk5hmGY5XPXTA7K9eCznKvozxx.jpg",
+            address: "인천 부평구 부평동",
+          },
+          {
+            id: "65ee9ca2b2e9a2001dc1f7e3",
+            gymName: "부평남부 바디홀릭 필라테스",
+            listImgUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/18e2c13da7355cff1/2ZwYTEgcGbZ7qNpRvcZ4bBEQvCRn7CCmFdff.센터 Small 복사.jpg",
+            address: "인천 부평구 부평동",
+          },
+          {
+            id: "654c5a795b1869001db38133",
+            gymName: "부평 스페시맨GYM",
+            listImgUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/18bb2416b9a4aa6cfe/i7hkxkCheAG42NgYuKnRvg7xJekS6N31fV.Small_231121_부평_스페시멘GYM_11.jpg",
+            address: "인천 부평구 부평동",
+          },
+          {
+            id: "5a73c5aa6959a655ea649cdb",
+            gymName: "부평 더블유짐",
+            listImgUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/161543c22ff45b4645/4K6uVdbJnWndJAmYHdfhiJGFKMyGznaShXc7ckxGwPma.jpg",
+            address: "인천 부평구 부평동",
+          },
+          {
+            id: "61936a38e271c900398564e1",
+            gymName: "부평 PT의정석 운동재활센터",
+            listImgUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/17d27d6ecb9117de10/4K7y1LZk6uGHeY8uKFvvoerx8vAzcMhZhj9HHeW8fBgt.jpg",
+            address: "인천 부평구 부평동",
+          },
+          {
+            id: "6386b97eb54b10001dec4fe3",
+            gymName: "부평 mgym",
+            listImgUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/184c64497a13d462cd/NV8xnm3W7Lz91cpfB6BB5qKjzHUe7Ws1PP.list_photo.jpg",
+            address: "인천 부평구 부평동",
+          },
+          {
+            id: "615e6c826bdf9700390c9e95",
+            gymName: "부개 골드짐",
+            listImgUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/17c58d7dcf52f4d98d/4K7xorRJgwgywmRHpZMi7K7cwoVQDFHobqagY4tZmiBi.jpg",
+            address: "인천 부평구 부개동",
+          },
+          {
+            id: "64d0d308d7c1bd001d65b785",
+            gymName: "부평 키워드핏",
+            listImgUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/189cfb859c73854ec6/2WQemZ31hyjcNCtCMSpqjQvFPBsakJxSXmp3.list_photo.jpg",
+            address: "인천 부평구 부평동",
+          },
+          {
+            id: "60c0a19ac70b9e003995a855",
+            gymName: "부개 팀타이거복싱짐",
+            listImgUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/179f0774361ef22f2/kXYbCnxsTYFHiFCvUChrcYKP1MrhMobhTw2sqAaWLQ.jpg",
+            address: "인천 부평구 부개동",
+          },
+          {
+            id: "637c6f2be38504001de06524",
+            gymName: "부개 복싱인사이드짐",
+            listImgUrl:
+              "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/1849e12404f415c4a3/MCvy7chq1vAp8KtHAd7DZ37mebt5DzVPd8.list_photo.jpg",
+            address: "인천 부평구 부개동",
+          },
+        ],
+      },
+    },
+  ];
+}
