d137620f814468355ecfb500de5de7aeefb07b88
NeverGround <167948341+NeverGround@users.noreply.github.com>
2024-06-21 12:52:19 +0900

    Add files via upload

---
+++ mysqlDB.ts
@@ -0,0 +1,32 @@
+// import express, { Request, Response } from "express";
+
+// const app = express();
+
+// app.listen(3001, () => {
+//   console.log("서버 실행함!");
+// });
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("하이 피싱사이트!");
+// });
+
+// app.get("/cookie", (req: Request, res: Response) => {
+//   res.send("쿠키");
+// });
+
+// const express = require("express");
+// const mysql = require("mysql2");
+// const app = express();
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "qwer1234",
+//   database: "reactspring",
+// });
+
+// connection.connect();
+// connection.query(
+//   `inert into coffee (id,name,price,calories) values (6,"밀크티",500,300)`
+// );
+// connection.end();
